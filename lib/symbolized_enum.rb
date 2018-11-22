8973b2720f7dda8e79f83f4239e20b024634829d
Will Leonard <wleonard@salsify.com>
2018-11-22 12:16:39 -0500

    Basic test and implementation

--- lib/symbolized_enum.rb
+++ lib/symbolized_enum.rb
@@ -7,9 +7,38 @@ module SymbolizedEnum
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def symbolized_enum(attr_name, predicates: false, predicate_generator: nil, **active_record_options)
+    def symbolized_enum(attr_name, predicates: false, prefixed_predicate: false, suffixed_predicate: false, predicate_name_generator: nil, **validates_inclusion_of_options)
       attribute(attr_name, :symbol)
-      validate_inclusion_of(attr_name, active_record_options)
+      validates_inclusion_of(attr_name, validates_inclusion_of_options)
+
+      return unless predicates
+
+      enum_values = validates_inclusion_of_options[:in]
+      if prefixed_predicate
+        enum_values.each do |enum_value|
+          define_method("#{attr_name}_#{enum_value}?") do
+            send(attr_name) == enum_value
+          end
+        end
+      elsif suffixed_predicate
+        enum_values.each do |enum_value|
+          define_method("#{enum_value}_#{attr_name}?") do
+            send(attr_name) == enum_value
+          end
+        end
+      elsif predicate_name_generator
+        enum_values.each do |enum_value|
+          define_method(predicate_name_generator.call(attr_name, enum_value)) do
+            send(attr_name) == enum_value
+          end
+        end
+      else
+        enum_values.each do |enum_value|
+          define_method("#{enum_value}?") do
+            send(attr_name) == enum_value
+          end
+        end
+      end
     end
   end
 end
